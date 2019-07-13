f3f3de2bfeefd0fba5b4e16c933e63b9e6d9e5a8
prashanth-g <prashanthcsgce@gmail.com>
2019-07-13 18:49:43 +0530

    Added test class for connector

---
+++ src/test/java/com/prashanth/kafka/github/source/connector/config/GithubSourceConnectorTest.java
@@ -0,0 +1,34 @@
+package com.prashanth.kafka.github.source.connector.config;
+
+import static com.prashanth.kafka.github.source.connector.config.GitHubSourceConnectorConfig.OWNER_CONFIG;
+import static com.prashanth.kafka.github.source.connector.config.GitHubSourceConnectorConfig.REPO_CONFIG;
+import static com.prashanth.kafka.github.source.connector.config.GitHubSourceConnectorConfig.SINCE_CONFIG;
+import static com.prashanth.kafka.github.source.connector.config.GitHubSourceConnectorConfig.TOPIC_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.BATCH_SIZE_CONFIG;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GithubSourceConnectorTest {
+
+  private Map<String, String> initialConfig() {
+    Map<String, String> initialProperties = new HashMap<>();
+    initialProperties.put(OWNER_CONFIG, "foo");
+    initialProperties.put(REPO_CONFIG, "bar");
+    initialProperties.put(SINCE_CONFIG, "2017-04-26T01:23:45Z");
+    initialProperties.put(BATCH_SIZE_CONFIG, "100");
+    initialProperties.put(TOPIC_CONFIG, "github-issues");
+    return initialProperties;
+  }
+
+  @Test
+  public void taskConfigsShouldReturnOneTaskConfig() {
+    GithubSourceConnector githubSourceConnector = new GithubSourceConnector();
+    githubSourceConnector.start(initialConfig());
+    assertEquals(githubSourceConnector.taskConfigs(1).size(), 1);
+    assertEquals(githubSourceConnector.taskConfigs(10).size(), 1);
+  }
+
+}
